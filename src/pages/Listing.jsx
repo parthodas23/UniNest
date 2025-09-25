2a71c6d54ca31bd40750b8e3d4bbce339f12e374
Partha Das <parthodasm@gmail.com>
2025-09-25 18:54:53 +0600

    Add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,6 +7,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/bundle";
 import { FaShareFromSquare } from "react-icons/fa6";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaBed } from "react-icons/fa";
+import { FaBath } from "react-icons/fa6";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
@@ -44,7 +47,7 @@ const Listing = () => {
           listing.imgUrls.map((url, index) => (
             <SwiperSlide key={index}>
               <div
-                className="w-fill overflow-hidden h-[300px]"
+                className="w-full overflow-hidden h-[300px]"
                 style={{
                   background: `url(${url}) center no-repeat`,
                   backgroundSize: "cover",
@@ -65,7 +68,50 @@ const Listing = () => {
       >
         <FaShareFromSquare />
       </div>
-      {shareLinkedCopied && <p className="fixed top-[20%] right-[1%] text-gray-200 rounded-xl p-1 bg-gray-500 z-10">Link Copied!</p>}
+      {shareLinkedCopied && (
+        <p className="fixed top-[20%] right-[1%] text-gray-200 rounded-xl p-1 bg-gray-500 z-10">
+          Link Copied!
+        </p>
+      )}
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-2xl shadow-md lg:space-x-4 z-10 overflow-x-hidden">
+        <div className="bg-gray-200 w-full h-[200px] lg-[400px] rounded-xl p-2">
+          <p className="text-2xl font-bold mb-3 text-gray-600">
+            {listing.title} - {listing.rent}(tk)
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            {" "}
+            <FaMapMarkerAlt className="text-green-600 mr-1" /> {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="w-full bg-red-700 max-w-[200px] text-purple-200 text-center p-1 rounded-md font-semibold shadow-md">
+              {listing.type === "For-Rent" ? "For-Rent" : "Looking-for-Room"}
+            </p>
+            <p className="w-full max-w-[200px] bg-green-600 text-purple-200 font-semibold text-center p-1 rounded-md shadow-md">
+              {"Roomtype: "}
+              {listing.roomType === "shared"
+                ? "Shared"
+                : listing.roomType === "Single"
+                ? "Single"
+                : "APertment"}
+            </p>
+          </div>
+          <p className="mt-3">
+            <span className="font-semibold">Description -</span>{" "}
+            {listing.description}
+          </p>
+          <ul className="flex justify-start space-x-10">
+            <li className="flex items-center font-semibold mt-3 whitespace-nowrap">
+              <FaBed className="text-lg mr-2" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center font-semibold mt-3 whitespace-nowrap">
+              <FaBath className="text-lg mr-2" />
+              {+listing.bathrooms > 1 ? `${listing.bedrooms} Baths` : "1 Bath"}
+            </li>
+          </ul>
+        </div>
+        <div className="bg-blue-300 w-full h-[200px] lgh-[400px]"></div>
+      </div>
     </main>
   );
 };
