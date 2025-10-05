a92e82cc2b5b737cff1e273f381abec834ee8002
Partha Das <parthodasm@gmail.com>
2025-10-05 18:37:41 +0600

    Message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,19 +27,25 @@ const Contact = ({ userRef, listing }) => {
         <div className="flex flex-col w-full">
           <p className="text-md mb-2 font-semibold">
             Contact <span className="font-bold">{landLord.name}</span> for{" "}
-            <span className="font-bold">{listing.title.toLowerCase()}</span>{" "}
-            :
+            <span className="font-bold">{listing.title.toLowerCase()}</span> :
           </p>
-          <textarea className="w-full text-xl text-gray-600 border-2 px-4 py-2 border-gray-300 rounded"
+          <textarea
+            className="w-full text-xl text-gray-600 border-2 px-4 py-2 border-gray-300 rounded"
             name="message"
             id="message"
             rows="2"
+            placeholder="Message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           ></textarea>
           <a
             href={`mailto:${landLord.email}?Subject=${listing.title}&body=${message}`}
-          > <button className=" mt-3 cursor-pointer px-7 py-2 bg-blue-300 rounded w-full hover:bg-blue-500 shadow-md">Send Message</button></a>
+          >
+            {" "}
+            <button className=" mt-3 cursor-pointer px-7 py-2 bg-blue-300 rounded w-full hover:bg-blue-500 shadow-md">
+              Send Message
+            </button>
+          </a>
         </div>
       )}
     </>
