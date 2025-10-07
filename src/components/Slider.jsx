de27d78cfb1ecb00392d50702024f06c9c1d2ef2
Partha Das <parthodasm@gmail.com>
2025-10-07 20:50:41 +0600

    Create slider components and add it to Home Page

---
+++ src/components/Slider.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import Spinner from "../components/Spinner";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination, Navigation } from "swiper/modules";
+import { useNavigate } from "react-router-dom";
+
+const Slider = () => {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingRef = collection(db, "listings");
+      const q = query(listingRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+      let listing = [];
+      querySnap.forEach((doc) => {
+        return listing.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listing);
+      console.log(listing);
+      setLoading(false);
+    };
+    fetchListings();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+  if (listings.length === 0) {
+    return <></>;
+  }
+  return (
+    listings && (
+      <>
+        <Swiper
+          slidesPerView={1}
+          effect="fade"
+          autoplay={{ delay: 2000 }}
+          navigation
+          pagination={{ type: "progressbar" }}
+          modules={[Autoplay, EffectFade, Pagination, Navigation]}
+        >
+          {listings.map((listing) => (
+            <SwiperSlide
+              key={listing.id}
+              onClick={() =>
+                navigate(`/category/${listing.data.type}/${listing.id}`)
+              }
+            >
+              <div
+                className="w-full relative overflow-hidden h-[300px]"
+                style={{
+                  background: `url(${listing.data.imgUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+              <p className="absolute text-red-800 left-1 top-3 font-bold">{listing.data.title}</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+};
+
+export default Slider;
